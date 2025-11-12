481cb46f514331c3f353e50ec8b25b6006dc7392
lambda liebert <longqt321@gmail.com>
2025-11-12 15:12:26 +0700

    added cmp-nvim-lsp

--- lua/config/plugins/nvim-cmp.lua
+++ lua/config/plugins/nvim-cmp.lua
@@ -79,5 +79,16 @@ return {
 				},
 			}),
 		})
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+		require("lspconfig").clangd.setup({
+			capabilities = capabilities,
+		})
+		require("lspconfig").lua_ls.setup({
+			capabilities = capabilities,
+		})
+		require("lspconfig").pyright.setup({
+			capabilities = capabilities,
+		})
 	end,
 }
