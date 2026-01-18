a0457b9955bca369fff9f8c858ce8c1366e87af7
lambda liebert <longqt321@gmail.com>
2026-01-18 17:17:07 +0700

    refactor: moved keymaps to local buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,6 @@
 vim.g.mapleader = " "
 local map = vim.keymap.set
 
-
 -- Global map
 map("n", "ww", ":w<CR>", { desc = "Save file" })
 map("n", "qq", ":q<CR>", { desc = "Quit" })
@@ -40,14 +39,6 @@ map("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Telescope live grep
 map("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "Telescope buffers" })
 map("n", "<leader>fh", ":Telescope help_tags<CR>", { desc = "Telescope help tags" })
 map("n", "<leader>fc", ":Telescope current_buffer_tags<CR>", { desc = "Tags in current buffer" })
-
-map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
-map("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
-map("n", "gr", vim.lsp.buf.references, { desc = "Find references" })
-map("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
-map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
-map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
 
 -- Diagnostics
 map("n", "[d", vim.diagnostic.goto_prev, { desc = "Prev diagnostic" })
--- lua/config/plugins/lsp/lsp-config.lua
+++ lua/config/plugins/lsp/lsp-config.lua
@@ -6,10 +6,35 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 	},
 	config = function()
+		-- Lấy capabilities từ nvim-cmp để LSP server hiểu
+
+		local on_attach = function(client, bufnr)
+			local opts = { buffer = bufnr, noremap = true, silent = true }
+			local keymap = vim.keymap.set
+
+			opts.desc = "Go to definition"
+			keymap("n", "gd", vim.lsp.buf.definition, opts)
+
+			opts.desc = "Hover documentation"
+			keymap("n", "K", vim.lsp.buf.hover, opts)
+
+			opts.desc = "Go to implementation"
+			keymap("n", "gi", vim.lsp.buf.implementation, opts)
+
+			opts.desc = "Go to declaration"
+			keymap("n", "gD", vim.lsp.buf.declaration, opts)
+
+			opts.desc = "Code action"
+			keymap({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+
+			opts.desc = "Rename symbol"
+			keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+			opts.desc = "Show references"
+			keymap("n", "gr", require("telescope.builtin").lsp_references, opts) -- Tối ưu: Dùng Telescope để xem references đẹp hơn
+		end
 		local lspconfig = require("lspconfig")
 		local mason_lspconfig = require("mason-lspconfig")
-
-		-- Lấy capabilities từ nvim-cmp để LSP server hiểu
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 		mason_lspconfig.setup({
@@ -19,12 +44,14 @@ return {
 				function(server_name)
 					lspconfig[server_name].setup({
 						capabilities = capabilities,
+						on_attach = on_attach,
 					})
 				end,
 				-- Cấu hình riêng cho Lua để sửa lỗi "Undefined global vim"
 				["lua_ls"] = function()
 					lspconfig.lua_ls.setup({
 						capabilities = capabilities,
+						on_attach = on_attach,
 						settings = {
 							Lua = {
 								diagnostics = { globals = { "vim" } },
