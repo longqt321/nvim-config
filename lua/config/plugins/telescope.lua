e3b04dd6c9dd4d48059b39c34e3e89ed9aadcf50
lambda liebert <longqt321@gmail.com>
2026-01-18 17:28:41 +0700

    optimize grep arguments

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -16,9 +16,19 @@ return {
 					path_display = { "smart" },
 					file_ignore_patterns = { "node_modules", "./git", "build/", "venv" },
 					sorting_strategy = "ascending",
-
 					layout_config = {
 						horizontal = { preview_width = 0.55 },
+					},
+					vimgrep_arguments = {
+						"rg",
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+						"--hidden",
+						"--glob=!.git/",
 					},
 				},
 
