c504011d527e84c51854f7a5c71c31cc84b61181
lambda liebert <longqt321@gmail.com>
2026-01-18 18:41:26 +0700

    updated: auto close nvim-tree

--- lua/config/plugins/nvim-tree.lua
+++ lua/config/plugins/nvim-tree.lua
@@ -15,6 +15,32 @@ return {
 					update_root = false,
 				},
 			})
+
+			-- TỰ ĐỘNG ĐÓNG: Auto Close NvimTree
+			-- Logic: Kiểm tra khi chuẩn bị thoát (QuitPre).
+			-- Nếu cửa sổ cuối cùng còn lại là NvimTree thì đóng nó luôn.
+			vim.api.nvim_create_autocmd("QuitPre", {
+				callback = function()
+					local tree_wins = {}
+					local floating_wins = {}
+					local wins = vim.api.nvim_list_wins()
+					for _, w in ipairs(wins) do
+						local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+						if bufname:match("NvimTree_") ~= nil then
+							table.insert(tree_wins, w)
+						end
+						if vim.api.nvim_win_get_config(w).relative ~= "" then
+							table.insert(floating_wins, w)
+						end
+					end
+					if 1 == #wins - #floating_wins - #tree_wins then
+						-- Đóng tất cả các cửa sổ NvimTree còn lại
+						for _, w in ipairs(tree_wins) do
+							vim.api.nvim_win_close(w, true)
+						end
+					end
+				end,
+			})
 		end,
 	},
 }
